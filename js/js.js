fc5655a7ec485dad23f6817da87bfc16ebdd479a
Maayan Elgamil <maayan.elgamil@gmail.com>
2017-05-08 14:30:50 +0300

    add noScroll

--- js/js.js
+++ js/js.js
@@ -64,6 +64,13 @@ function dialogRGdown() {
 
 //move between div
 $(document).ready(function () {
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
     /*#region DivFunctions*/
     $('#regDiv').hide();
     $('#loginDiv').hide();
